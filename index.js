a9abffc86a9fcbca74fe5aab5527d23495bcaad6
Henry <henrywtam@gmail.com>
2023-05-20 14:34:39 -0700

    Added small UI to this

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import { Configuration, OpenAIApi } from "openai";
 import express from "express";
 import bodyParser from "body-parser";
@@ -18,7 +23,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.sendFile("index.html" , { root : __dirname});
+});
+
 app.post("/", async (req, res) => {
+    const { messages } = req.body;
+    console.log(messages);
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+            { role: "system", content: 'You are a MagicGPT helpful assistant Magic: the Gathering chatbot' },
+            ...messages
+        ]
+    })
+
+    res.json({
+        completion: completion.data.choices[0].message
+    })
+});
+
+app.get("/", async (req, res) => {
     const { messages } = req.body;
     console.log(messages);
     const completion = await openai.createChatCompletion({
